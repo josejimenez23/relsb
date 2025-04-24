cda873f372bbabc6958148c77fff728b88b6be5f
josejimenez23 <manueljimenez2397@gmail.com>
2025-04-24 10:30:30 +0200

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // Cargar la librería AOS (estilos y script)
+  const cargarAOS = () => {
+    // Crear y añadir el enlace de los estilos de AOS
+    const enlace = document.createElement('link');
+    enlace.rel = 'stylesheet';
+    enlace.href = 'https://unpkg.com/aos@2.3.4/dist/aos.css';
+    document.head.appendChild(enlace);
+
+    // Crear y añadir el script de AOS
+    const script = document.createElement('script');
+    script.src = 'https://unpkg.com/aos@2.3.4/dist/aos.js';
+    script.onload = () => AOS.init({ duration: 800, once: true }); // Iniciar AOS después de cargar
+    document.body.appendChild(script);
+  };
+
+  cargarAOS(); // Ejecutar la función para cargar AOS
+
+  // Añadir el evento para hacer scroll hacia arriba cuando se hace clic en el botón
+  const botonArriba = document.getElementById("boton-arriba");
+  if (botonArriba) {
+    botonArriba.addEventListener("click", () => {
+      window.scrollTo({ top: 0, behavior: "smooth" }); 
+    });
+  }
+});
